c9c6288b7e41f72e541bc9d461dd0d67c0066bec
luizzitol <62354883+luizzitol@users.noreply.github.com>
2020-04-09 14:45:05 +0100

    Updates

---
+++ precache-manifest.2648a71f6a546b9b7a24b8a62d9e9096.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "949ef7aa5910e6b45aefe05978d623bc",
+    "url": "/keepers-app/index.html"
+  },
+  {
+    "revision": "53f4e09ee04475204335",
+    "url": "/keepers-app/static/js/2.b993e5c5.chunk.js"
+  },
+  {
+    "revision": "0ed616fe55eb0fab9ff7",
+    "url": "/keepers-app/static/js/main.2505ce04.chunk.js"
+  },
+  {
+    "revision": "f06c4cf59333f34da959",
+    "url": "/keepers-app/static/js/runtime-main.afa7a62d.js"
+  }
+]);
